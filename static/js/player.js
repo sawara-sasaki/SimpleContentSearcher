de46a1a8645ac2ab985e850136ff8740a0f8f645
sawara-sasaki <sawara.sasaki20200730@gmail.com>
2020-10-23 18:19:57 +0900

    fix js

--- static/js/player.js
+++ static/js/player.js
@@ -1,17 +1,25 @@
 $(function(){
+  var vid = ""
   var qs = window.location.search;
   var qo = new Object();
-  if(qs){
-    qs = qs.substring(1);
-    var pa = qs.split('&');
-    for (var i = 0; i < pa.length; i++) {
-      var el = pa[i].split('=');
-      var pn = decodeURIComponent(el[0]);
-      var pv = decodeURIComponent(el[1]);
-      qo[pn] = pv;
+  if (qSearch.startsWith('?https://www.youtube.com/watch?v=')) {
+    vid = qSearch.substring(33).split('&')[0];
+  } else {
+    if(qs){
+      qs = qs.substring(1);
+      var pa = qs.split('&');
+      for (var i = 0; i < pa.length; i++) {
+        var el = pa[i].split('=');
+        var pn = decodeURIComponent(el[0]);
+        var pv = decodeURIComponent(el[1]);
+        qo[pn] = pv;
+      }
+    }
+    if (!!qo["v"] && qo["v"].length > 0) {
+      vid = qo["v"];
     }
   }
-  const data = {action:"search", parameters:["https://www.youtube.com/watch?v="+qo["v"]]}
+  const data = {action:"search", parameters:["https://www.youtube.com/watch?v="+vid]}
   request(data, (res)=>{
     if(res.data.length > 0) {
       showInfoMessage(res.data);
